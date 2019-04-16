52b25beb5af8f2ce02d81b134b8172aaf8113c0a
nick <n.sychevskyi@gmail.com>
2019-04-16 04:44:33 +0300

    Initial commit

--- app/scripts/color-generator.js
+++ app/scripts/color-generator.js
@@ -1,5 +1,4 @@
 const generateColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
 const updateColor = (element, updateTime) =>
-    setInterval(() =>
-        element.style.backgroundColor = generateColor(), updateTime);
+    setInterval(() => element.style.backgroundColor = generateColor(), updateTime);
